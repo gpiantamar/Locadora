bd45975773e9324dbe3a62950c27f08a50ee8dfb
Guilherme Silva Piantamar <124617631+gpiantamar@users.noreply.github.com>
2026-08-01 13:59:38 -0300

    Refactor Program.cs for clarity and structure
    Refactor Program.cs to simplify database context setup and improve comments.

--- CarLoc/Program.cs
+++ CarLoc/Program.cs
@@ -1,34 +1,31 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using CarLoc.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-if(builder.Environment.IsDevelopment())
-{
-    builder.Services.AddDbContext<CarLocContext>(options =>
-        options.UseSqlite(builder.Configuration.GetConnectionString("CarLocContext")));
-}
-else
-{
-     builder.Services.AddDbContext<CarLocContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionCarLocContext")));
-}
+// Banco de dados
+builder.Services.AddDbContext<CarLocContext>(options =>
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("CarLocContext")
+    )
+);
 
-
-// Add services to the container.
+// MVC
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuração do pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// IMPORTANTE:
+// Não usamos UseHttpsRedirection() no Render.
+// O HTTPS é tratado pelo próprio Render.
+
 app.UseStaticFiles();
 
 app.UseRouting();
